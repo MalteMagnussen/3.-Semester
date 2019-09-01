58abc0ce6d695fe7326d255491f616db63004f73
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2019-09-01 13:50:34 +0200

    Inital Push with a ton of changes.
    Lets see what happens.

---
+++ Week-36/00-monday-exercises/rest-jpa-devops-startcode/src/main/java/facades/MovieFacade.java
@@ -0,0 +1,57 @@
+package facades;
+
+import entities.Movie;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import entityUtils.EMF_Creator;
+
+
+/**
+ *
+ * Rename Class to a relevant name Add add relevant facade methods
+ */
+public class MovieFacade {
+
+    private static MovieFacade instance;
+    private static EntityManagerFactory emf;
+    
+    //Private Constructor to ensure Singleton
+    private MovieFacade() {}
+    
+    
+    /**
+     * 
+     * @param _emf
+     * @return an instance of this facade class.
+     */
+    public static MovieFacade getFacadeExample(EntityManagerFactory _emf) {
+        if (instance == null) {
+            emf = _emf;
+            instance = new MovieFacade();
+        }
+        return instance;
+    }
+
+    // private EntityManager getEntityManager() {
+    //     return emf.createEntityManager();
+    // }
+    
+    //TODO Remove/Change this before use
+    public long getRenameMeCount(){
+        EntityManager em = emf.createEntityManager();
+        try{
+            long renameMeCount = (long)em.createQuery("SELECT COUNT(r) FROM RenameMe r").getSingleResult();
+            return renameMeCount;
+        }finally{  
+            em.close();
+        }
+        
+    }
+
+    public static void main(String[] args) {
+        MovieFacade fe = getFacadeExample(EMF_Creator.getEMF(EMF_Creator.Strategy.DROP_AND_CREATE));
+        long count = fe.getRenameMeCount();
+        System.out.println("count: "+count);
+    }
+}
---
+++ Week-36/00-monday-exercises/rest-jpa-devops-startcode/src/test/java/rest/MovieResourceTest.java
@@ -0,0 +1,113 @@
+package rest;
+
+import entities.RenameMe;
+import utils.EMF_Creator;
+import io.restassured.RestAssured;
+import static io.restassured.RestAssured.given;
+import io.restassured.parsing.Parser;
+import java.net.URI;
+import java.util.Map;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.ws.rs.core.UriBuilder;
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.grizzly.http.util.HttpStatus;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+import static org.hamcrest.Matchers.equalTo;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import utils.EMF_Creator.DbSelector;
+import utils.EMF_Creator.Strategy;
+
+//Uncomment the line below, to temporarily disable this test
+//@Disabled
+public class MovieResourceTest {
+
+//    private static final int SERVER_PORT = 7777;
+//    private static final String SERVER_URL = "http://localhost/api";
+//    //Read this line from a settings-file  since used several places
+//    private static final String TEST_DB = "jdbc:mysql://localhost:3307/startcodev2-test";
+    private static final Map<String, String> PROPS = entityUtils.EMF_Creator.getProps();
+    static final URI BASE_URI = UriBuilder.fromUri(PROPS.get("test_server")).port(Integer.parseInt(PROPS.get("test_port"))).build();
+    private static HttpServer httpServer;
+    private static EntityManagerFactory emf;
+
+    static HttpServer startServer() {
+        System.out.println("BASE_URI: "+BASE_URI);
+        ResourceConfig rc = ResourceConfig.forApplication(new ApplicationConfig());
+        return GrizzlyHttpServerFactory.createHttpServer(BASE_URI, rc);
+    }
+
+    @BeforeAll
+    public static void setUpClass() {
+        //First Drop and Rebuild the test database 
+        emf = EMF_Creator.getEMF(EMF_Creator.Strategy.DROP_AND_CREATE);
+
+        //Set System property so the project executed by the Grizly-server wil use this same database
+        System.setProperty("IS_TEST", PROPS.get("connection"));
+        //We are using the database on the virtual Vagrant image, so username password are the same for all dev-databases
+        
+        httpServer = startServer();
+        
+        //Setup RestAssured
+        RestAssured.baseURI = PROPS.get("test_server");
+        RestAssured.port = Integer.parseInt(PROPS.get("test_port"));
+        
+        RestAssured.defaultParser = Parser.JSON;
+    }
+    
+    @AfterAll
+    public static void closeTestServer(){
+        //System.in.read();
+         httpServer.shutdownNow();
+    }
+    
+    // Setup the DataBase (used by the test-server and this test) in a known state BEFORE EACH TEST
+    //TODO -- Make sure to change the script below to use YOUR OWN entity class
+    @BeforeEach
+    public void setUp() {
+        EntityManager em = emf.createEntityManager();
+        try {
+            em.getTransaction().begin();
+            em.createNamedQuery("RenameMe.deleteAllRows").executeUpdate();
+
+            em.persist(new RenameMe("Some txt","More text"));
+            em.persist(new RenameMe("aaa","bbb"));
+           
+            em.getTransaction().commit();
+        } finally {
+            em.close();
+        }
+    }
+    
+    @Test
+    public void testServerIsUp() {
+        System.out.println("Testing is server UP");
+        given().when().get("/xxx").then().statusCode(200);
+    }
+   
+    //This test assumes the database contains two rows
+    @Test
+    public void testDummyMsg() throws Exception {
+        given()
+        .contentType("application/json")
+        .get("/xxx/").then()
+        .assertThat()
+        .statusCode(HttpStatus.OK_200.getStatusCode())
+        .body("msg", equalTo("Hello World"));   
+    }
+    
+    @Test
+    public void testCount() throws Exception {
+        given()
+        .contentType("application/json")
+        .get("/xxx/count").then()
+        .assertThat()
+        .statusCode(HttpStatus.OK_200.getStatusCode())
+        .body("count", equalTo(2));   
+    }
+}
