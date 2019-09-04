e1b498d3cd843339772725124fd4bd1a79cb0421
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2019-09-04 13:35:08 +0200

    Wednesday Exercise 01 done.

--- Week-36/02-wednesday-exercises/dag1-js/src/a.js
+++ Week-36/02-wednesday-exercises/dag1-js/src/a.js
@@ -30,3 +30,25 @@ all.shift("Hans");
 //g) REMOVE FROM THE END OF THE ARRAY
 all.pop("Gitte");
 //console.log(all);
+
+//h) SPLICE
+all.splice(3,2);
+//console.log(all);
+
+//i) REVERSE
+all.reverse();
+//console.log(all);
+
+//j) && k) SORT
+all.sort(function (a, b) {
+  return a.localeCompare(b);
+});
+//
+
+//l) MAP
+var all2 = all.map(name => name.toUpperCase());
+//console.log(all2);
+
+//m) FILTER
+var all3 = all.filter(name => name.charAt(0) === "L" || name.charAt(0) === "L");
+//console.log(all3);
