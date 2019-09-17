ad3170bf07338f82764faa23fc66d552709e576d
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2019-09-17 13:09:16 +0200

    Initial Tuesday Exercise Push

---
+++ Week-38/01-tuesday-exercises/CollectionsOfBasicTypes/src/main/java/entities/Customer.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entities;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Malte
+ */
+@Entity
+public class Customer implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private List<String> hobbies = new ArrayList<>();
+
+    public Customer() {
+    }
+    
+    public void addHobby(String s) {
+        hobbies.add(s);
+    }
+    
+    public String getHobbies() {
+        String returnString = "";
+        for(String hobby: hobbies) {
+            returnString += hobby + ", ";
+        }
+        return returnString;
+    }
+
+    /**
+     * Get the value of lastName
+     *
+     * @return the value of lastName
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * Set the value of lastName
+     *
+     * @param lastName new value of lastName
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+
+    /**
+     * Get the value of firstName
+     *
+     * @return the value of firstName
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * Set the value of firstName
+     *
+     * @param firstName new value of firstName
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    
+    
+}
