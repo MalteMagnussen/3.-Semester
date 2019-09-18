5076a2b47b12f97457ef83afaeaf735ff0524b81
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2019-09-18 09:28:45 +0200

    Wednesday Exercise Initial Push

---
+++ Week-38/02-wednesday-exercises/RESTwithJAXRS/src/test/java/facades/FacadeExampleTest.java
@@ -0,0 +1,58 @@
+package facades;
+
+import utils.EMF_Creator;
+import entities.Person;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Query;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import utils.Settings;
+import utils.EMF_Creator.DbSelector;
+import utils.EMF_Creator.Strategy;
+
+public class FacadeExampleTest {
+
+    private static EntityManagerFactory emf;
+    private static PersonFacade facade;
+
+    public FacadeExampleTest() {
+    }
+
+    @BeforeAll
+    public static void setUpClassV2() {
+        emf = EMF_Creator.createEntityManagerFactory(DbSelector.TEST, Strategy.DROP_AND_CREATE);
+        facade = PersonFacade.getPersonFacade(emf);
+    }
+
+    @AfterAll
+    public static void tearDownClass() {
+    }
+
+    @BeforeEach
+    public void setUp() {
+        EntityManager em = emf.createEntityManager();
+        try {
+            em.getTransaction().begin();
+
+            em.getTransaction().commit();
+        } finally {
+            em.close();
+        }
+    }
+
+    @AfterEach
+    public void tearDown() {
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+        Query query = em.createNativeQuery("truncate table CA1_test.STUDENT;"); // TODO RENAME
+        query.executeUpdate();
+        em.getTransaction().commit();
+    }
+
+}
---
+++ Week-38/02-wednesday-exercises/RESTwithJAXRS/src/test/java/rest/RenameMeResourceTest.java
@@ -0,0 +1,74 @@
+package rest;
+
+import utils.EMF_Creator;
+import io.restassured.RestAssured;
+import io.restassured.parsing.Parser;
+import java.net.URI;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Query;
+import javax.ws.rs.core.UriBuilder;
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import utils.EMF_Creator.DbSelector;
+import utils.EMF_Creator.Strategy;
+
+public class RenameMeResourceTest {
+
+    private static final int SERVER_PORT = 7777;
+    private static final String SERVER_URL = "http://localhost/api";
+
+    static final URI BASE_URI = UriBuilder.fromUri(SERVER_URL).port(SERVER_PORT).build();
+    private static HttpServer httpServer;
+    private static EntityManagerFactory emf;
+
+    static HttpServer startServer() {
+        ResourceConfig rc = ResourceConfig.forApplication(new ApplicationConfig());
+        return GrizzlyHttpServerFactory.createHttpServer(BASE_URI, rc);
+    }
+
+    @BeforeAll
+    public static void setUpClass() {
+        emf = EMF_Creator.createEntityManagerFactory(DbSelector.TEST, Strategy.CREATE);
+
+        httpServer = startServer();
+
+        RestAssured.baseURI = SERVER_URL;
+        RestAssured.port = SERVER_PORT;
+
+        RestAssured.defaultParser = Parser.JSON;
+    }
+
+    @AfterAll
+    public static void closeTestServer() {
+        httpServer.shutdownNow();
+    }
+
+    @BeforeEach
+    public void setUp() {
+        EntityManager em = emf.createEntityManager();
+        try {
+            em.getTransaction().begin();
+
+            em.getTransaction().commit();
+        } finally {
+            em.close();
+        }
+    }
+
+    @AfterEach
+    public void tearDown() {
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+        Query query = em.createNativeQuery("truncate table CA1_test.STUDENT;"); // TODO RENAME
+        query.executeUpdate();
+        em.getTransaction().commit();
+    }
+
+}
