057b62e7fd9aaf8cf8468f655b30e2fcd2ceb349
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2019-09-23 16:14:57 +0200

    Tuesday Exercise - 3) red

--- Week-39/01-tuesday-exercises/tuesday01.js
+++ Week-39/01-tuesday-exercises/tuesday01.js
@@ -63,3 +63,27 @@ function myMap(array, callback) {
 
 console.log(myMap(namesArray, reverseString));
 
+// 3) Using the Prototype property to add new functionality to existing objects
+// Every JavaScript function has a prototype property (this property is empty by default), 
+// and you can attach properties and methods on this prototype property. 
+// You add methods and properties on an object’s prototype property to make those methods and properties available to all instances of that Object. 
+// You can even implement (classless) inheritance hierarchies with this property.
+// The problem with our two user defined functions above (myFilter and myMap) is that they are not really attached to the Array Object. 
+// They are just functions, where we have to pass in both the array and the callback.
+// Create a new version of the two functions (without the array argument) which you should add to the Array prototype property 
+// so they can be called on any array as sketched below:
+// var names = ["Lars", "Peter", "Jan", "Bo"];
+// var newArray = names.myFilter(function(name) {…});
+
+Array.prototype.myFilter = function (callback) {
+    var returnArray = [];
+    this.forEach(element => {
+        if (!(callback(element) == undefined)) {
+            returnArray.push(callback(element));
+        }
+    });
+    return returnArray;
+};
+
+console.log('3) Using the Prototype property to add new functionality to existing objects');
+console.log(namesArray.myFilter(filterNames));
