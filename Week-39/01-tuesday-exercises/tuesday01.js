1095952baea59a633e195143796e92afc92acfa6
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2019-09-23 16:04:18 +0200

    Tuesday Exercise - Yellow 2)

--- Week-39/01-tuesday-exercises/tuesday01.js
+++ Week-39/01-tuesday-exercises/tuesday01.js
@@ -25,5 +25,41 @@ function reverseString(name) {
 
 console.log(namesArray.map(reverseString));
 
+// 2)       Implement user defined functions that take callbacks as an argument
+// Now, assume the array did not offer these two methods. Then we would have to implement them by our self. 
+// a) Implement a function: myFilter(array, callback) that takes an array as the first argument, 
+// and a callback as the second and returns a new (filtered) array according to the code provided in the callback 
+// (this method should provide the same behaviour as the original filter method).
+// Test the method with the same array and callback as in the example with the original filter method.
 
+function myFilter(array, callback) {
+    var returnArray = [];
+    array.forEach(element => {
+        if (!(callback(element) == undefined)) {
+            returnArray.push(callback(element));
+        }
+    });
+    return returnArray;
+}
+
+console.log('a) Implement a function: myFilter(array, callback) that takes an array as the first argument');
+console.log(myFilter(namesArray, filterNames));
+
+// b) Implement a function: myMap(array, callback)that, provided an array and a callback, 
+// provides the same functionality as calling the existing map method on an array.
+// Test the method with the same array and callback as in the example with the original map method.
+
+console.log('b) Implement a function: myMap(array, callback)that, provided an array and a callback, ');
+
+function myMap(array, callback) {
+    var returnArray = [];
+    array.forEach(element => {
+        if (!(callback(element) == undefined)) {
+            returnArray.push(callback(element));
+        }
+    });
+    return returnArray;
+}
+
+console.log(myMap(namesArray, reverseString));
 
