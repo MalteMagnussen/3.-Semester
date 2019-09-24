5958d9a1883d6aa61d8c719e51b3f4b29e476f71
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2019-09-24 11:42:15 +0200

    Tuesday Exercise work

--- Week-39/01-tuesday-exercises/tuesday01/src/main/webapp/tuesday02.js
+++ Week-39/01-tuesday-exercises/tuesday01/src/main/webapp/tuesday02.js
@@ -106,7 +106,7 @@ console.log(reduceVotes());
 // Team up with another member of the class. Read about hoisting and implement at least two examples (individually) to illustrate that:
 // 1) Function declarations are completely hoisted
 
-printHoist = function(number) {
+printHoist = function (number) {
     return number;
 }
 console.log('Function declarations are completely hoisted');
@@ -140,4 +140,34 @@ hoistingExample02 = 5;
 // 3) What is the difference between the keyword var and the ES6 keyword let?
 
 // let is not hoisted. 
+
+// this in JavaScript
+// Read: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/this 
+// Team up with another member of the class. Read about this in JavaScript 
+// and implement at least three examples (individually) to illustrate how this in JavaScript differs from what we know from Java. 
+
+console.log('Three examples of this.');
+console.log('call()');
+var obj = { name: "Malte" };
+var hello = function (a, b, c) {
+    return 'Hello! ' + this.name + ' and let us also greet the following: ' + a + ' and ' + b + ' and last but not least ' + c;
+}
+console.log(hello.call(obj, "Call01", "Call02", "Call03"));
+
+console.log('apply()');
+var args = ['Apply01', 'Apply02', 'Apply03'];
+console.log(hello.apply(obj, args));
+
+console.log('bind()');
+
+// One of the examples should include an example of explicit setting this using either call(), apply() or bind().
+// Explain to each other, using the examples (as if it was the exam):
+// How this in JavaScript differ from this in Java:
+
+// This in javascript refers to the object it belongs to. But in Java you're always coding in an Object. 
+// So you always know what this refers to. That is a bit harder to know in JavaScript right now. 
+// JavaScripts this is very context related. It can refer to global object or other things depending on the context.
+// I don't quite understand this in JavaScript yet, and I'll have to work more with it to get it. 
+
+// The purpose of the methods call(), apply() and bind()
 
