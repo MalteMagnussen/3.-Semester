d9af18a24feb605c31f0781f1f5805806cef4ae0
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2019-09-24 18:43:35 +0200

    Fuck onsdagsopgaven. Gammelt materiale der ikke er blevet opdateret. Mange ting vi ikke har lært i det.

--- Week-39/02-wednesday-exercises/code_simple_SPA-master/src/index.js
+++ Week-39/02-wednesday-exercises/code_simple_SPA-master/src/index.js
@@ -1,18 +1,34 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import "isomorphic-fetch"
 
-var myFetch = function (URL, element) {
+var myFetch = function (URL) {
     fetch(URL)
         .then(response => response.json())
         .then(data => {
-            element = data.joke;
+            document.getElementById("myDiv").innerHTML = data.joke;
         })
 }
 
 var jokeURL = 'https://studypoints.info/jokes/api/jokes/period/hour';
 
 var getJoke = function () {
-    return myFetch(jokeURL, document.getElementById("myDiv").innerHTML);
+    return myFetch(jokeURL);
 }
 
 document.getElementById("myButton").addEventListener("click", getJoke);
+
+// 7) Change the functionality to get a new quote every hour. (Hint: use setInterval() )
+
+// I don't understand this task. Like, if you click the button, and the JSON endpoint has a new joke, then it shows the new joke.
+// The joke has changed while I was working on this. Like, it takes a joke from the endpoint.
+// And the endpoint changes jokes by itself every hour. I dont understand what they want here. 
+
+// Use developer-tools in your browser and it’s network options to monitor the AJAX-request. 
+// Explain why, what you did above, is even possible, 
+// when we know the Same Origin Policy governs when/where AJAX-request can go
+
+// Imma be real with you. 
+// I have no fucking clue what "Same Origin Policy" even is. 
+// And I've read several webpages about it so far. 
+// Read about it here: https://en.wikipedia.org/wiki/Same-origin_policy
+// It is something about security. 
