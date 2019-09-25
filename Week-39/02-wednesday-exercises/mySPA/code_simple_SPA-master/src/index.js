84845ae76bd315a789f6b9de7b98360ebc020c8c
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2019-09-25 18:45:23 +0200

    undefined hell

--- Week-39/02-wednesday-exercises/mySPA/code_simple_SPA-master/src/index.js
+++ Week-39/02-wednesday-exercises/mySPA/code_simple_SPA-master/src/index.js
@@ -1,30 +1,8 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import jokes from "./jokes";
 
-// Use JavaScript, fetch and DOM-manipulation to create a SPA with the following functionality:
-// 1) Show all users (in a table) - GET
-// 2) Show a single user, given an ID - GET BY ID
-// 3) Add a new User - POST
-// 4) Edit an existing user - PUT
-// 5) Delete an existing user - DELETE
-// You should only use the index.html file and the index.js file in the src-folder. 
-
 /**
- * Basic Get requests. 
- * @param String URL 
- */
-var basicFetchGet = function (URL, callback) {
-    fetch(URL)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            callback(data);
-        });
-}
-
-/**
- * Fetch with Error Handling
+ * Fetch with Error Handling - Help function
  * @param String URL 
  * @param function callback 
  */
@@ -41,9 +19,11 @@ var errorHandlingFetch = function (URL, callback) {
         .then(data => callback(data))
         .catch(err => {
             if (err.status) {
-                err.fullError.then(e => callback(e.detail))
+                var error = getErrorTable(err.fullError);
+                document.getElementById("content").innerHTML = error;
+                err.fullError.then(e => console.log(e.detail))
             }
-            else { callback("Network error"); }
+            else { console.log("Network error"); }
         });
 }
 
@@ -51,7 +31,7 @@ var errorHandlingFetch = function (URL, callback) {
 // For requests other than GET,  you need to provide fetch with an object containing the options for the request you plan to do. 
 // As an example, this is how you could make a POST request:
 /**
- * POST PUT DELETE Requests. 
+ * POST PUT DELETE Requests. Help function
  * @param String URL 
  * @param String method - POST PUT DELETE  
  * @param Person body 
@@ -72,5 +52,66 @@ function request(URL, method, body) {
     return fetch(URL, makeOptions(method, body));
 }
 
+/**
+ * Homemade Table Helper function for all users. 
+ * @param data 
+ */
+function makeTable(data) {
+    var tableData = data.map(person => "<tr><td>" + person.id + "</td><td>" + person.age + "</td><td>" + person.name + "</td><td>" + person.gender + "</td><td>" + person.email + "</td></tr>");
+    tableData.unshift('<table class="table"><tr><th scope="col">id</th><th scope="col">age</th><th scope="col">name</th><th scope="col">gender</th><th scope="col">email</th></tr>');
+    tableData.push("</table>");
+    return tableData.join("");
+}
 
+/**
+ * Home Helper function for 1 person. 
+ * @param data 
+ */
+function getOneUserTable(data) {
+    var tableData = ["<tr><td>" + data.id + "</td><td>" + data.age + "</td><td>" + data.name + "</td><td>" + data.gender + "</td><td>" + data.email + "</td></tr>"];
+    tableData.unshift('<table class="table"><tr><th scope="col">id</th><th scope="col">age</th><th scope="col">name</th><th scope="col">gender</th><th scope="col">email</th></tr>');
+    tableData.push("</table>");
+    return tableData.join("");
+};
 
+function getErrorTable(data) {
+    var tableData = ["<tr><td>" + data.status + "</td><td>" + data.msg + "</td></tr>"];
+    tableData.unshift('<table class="table"><tr><th scope="col">Status</th><th scope="col">Message</th></tr>');
+    tableData.push("</table>");
+    return tableData.join("");
+}
+
+// EXERCISES 
+// Use JavaScript, fetch and DOM-manipulation to create a SPA with the following functionality:
+// You should only use the index.html file and the index.js file in the src-folder. 
+
+// 1) Show all users (in a table) - GET
+/**
+ * Show All Users in a table - GET
+ */
+function getAllUsers() {
+    var URL = 'http://localhost:3333/api/users';
+    var content = document.getElementById("content");
+
+    errorHandlingFetch(URL, function (data) {
+        content.innerHTML = makeTable(data);
+    })
+}
+
+document.getElementById("getAllData").onclick = getAllUsers;
+
+// 2) Show a single user, given an ID - GET BY ID
+function getOneUser() {
+    var ID = document.getElementById("input").value;
+    var URL = 'http://localhost:3333/api/users/' + ID;
+    var content = document.getElementById("content");
+
+    errorHandlingFetch(URL, function (data) {
+        content.innerHTML = getOneUserTable(data);
+    })
+}
+document.getElementById("getOneUser").onclick = getOneUser;
+
+// 3) Add a new User - POST
+// 4) Edit an existing user - PUT
+// 5) Delete an existing user - DELETE
