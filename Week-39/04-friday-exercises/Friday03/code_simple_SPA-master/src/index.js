4e07b7653f13ee70b19dc9cf292483f632bd2552
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2019-09-27 12:17:34 +0200

    Friday last exercise work.

--- Week-39/04-friday-exercises/Friday03/code_simple_SPA-master/src/index.js
+++ Week-39/04-friday-exercises/Friday03/code_simple_SPA-master/src/index.js
@@ -1,8 +1,74 @@
-import 'bootstrap/dist/css/bootstrap.css'
-import jokes from "./jokes";
+// import 'bootstrap/dist/css/bootstrap.css'
 
-const allJokes = jokes.getJokes().map(joke => "<li>"+joke+"</li>");
-document.getElementById("jokes").innerHTML = allJokes.join("");
+var URI = 'http://uinames.com/api/';
+var table = document.getElementById('tblbody');
+var submitbutton = document.getElementById('btnsend');
+var sqlbutton = document.getElementById('btnsql');
+var sql = document.getElementById("sql");
+var dataStore = [];
+
+function getErrorTable(data) {
+    return "<tr><td>" + data.error + "</td></tr>";
+}
+
+function generateTable(data) {
+    var tableData = [];
+    //Body
+    data.map(function (value) {
+        tableData.push('<tr>');
+        tableData.push('<td>' + value.name + '</td>');
+        tableData.push('<td>' + value.surname + '</td>');
+        tableData.push('<td>' + value.gender + '</td>');
+        tableData.push('</tr>');
+    })
+
+    return tableData.join("");
+}
+
+var errorHandlingFetch = function (URL, callback) {
+    function handleHttpErrors(res) {
+        if (!res.ok) {
+            return Promise.reject({ status: res.status, fullError: res.json() })
+        }
+        return res.json();
+    }
+
+    fetch(URL)
+        .then(handleHttpErrors)
+        .then(data => callback(data))
+        .catch(err => {
+            if (err.status) {
+                err.fullError.then(e => console.log(e.detail))
+                err.fullError.then(e => sql.innerHTML = e.error)
+            }
+            else { console.log("Network error"); }
+        });
+}
+
+function personTable(data) {
+    dataStore += data;
+    table.innerHTML = generateTable(data);
+}
+
+function populateTable() {
+
+    var gender = document.getElementById('gender').value;
+    var region = document.getElementById('region').value;
+    var amount = document.getElementById('amount').value;
+    var url = "";
+    if (amount > 0) {
+        url += "?amount=" + amount;
+    }
+    if (gender !== "both") {
+        url += "?gender=" + gender;
+    }
+    if (region !== "all") {
+        url += "?region=" + region;
+    }
+
+    errorHandlingFetch(URI + url, personTable);
+}
 
 
 
+submitbutton.addEventListener("click", populateTable);
