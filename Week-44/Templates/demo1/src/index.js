5c78969c31a49f08341e9b4c632f3d0f967fae86
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2019-10-27 20:21:09 +0100

    Added multi-exercises in 1 project

--- Week-44/Templates/demo1/src/index.js
+++ Week-44/Templates/demo1/src/index.js
@@ -2,5 +2,46 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+//Create a new file App2.js (and App3.js ....)
+//In index.js, delete EVERYTHING BELOW the import of App and use the strategy below to switch between the smaller exercises
+//NOTE: THIS IS NOT THE WAY TO DO THINGS AFTER THE FIRST 2-3 DAYS
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import App2 from "./App2";
+
+let app = <App />;
+
+const DontUseMeForReal = () => {
+  return (
+    <div className="App" onClick={handleSelect}>
+      <a href="/" className="x" id="app1">
+        ex1
+      </a>{" "}
+      &nbsp;
+      <a href="/" className="x" id="app2">
+        ex2
+      </a>{" "}
+      &nbsp;
+      {/* Add as many as you have exercises, but remember className="x" */}
+      {app}
+    </div>
+  );
+};
+
+function handleSelect(event) {
+  event.preventDefault();
+  if (event.target.className !== "x") {
+    return;
+  }
+  const id = event.target.id;
+  switch (id) {
+    case "app1":
+      app = <App />;
+      break;
+    case "app2":
+      app = <App2 />;
+      break;
+  }
+  ReactDOM.render(<DontUseMeForReal />, document.getElementById("root"));
+}
+
+ReactDOM.render(<DontUseMeForReal />, document.getElementById("root"));
