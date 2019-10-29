1f92b347dc75050efa891b9230e0b91087f24484
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2019-10-29 12:21:27 +0100

    Install and import usestate

--- Week-44/01-tuesday-exercises/demo1/src/App.js
+++ Week-44/01-tuesday-exercises/demo1/src/App.js
@@ -1,5 +1,12 @@
-import React from "react";
+import React, { usestate } from "react";
 import "./App.css";
+
+/*
+1) Create a simple React component using the useState hook as described below
+
+a) It should have a button which when pressed should increment a count value, 
+initially by one, and print the result in the component.
+*/
 
 function App() {
   return (
