a72c12347adf7c0e8735ecdd65b42998a7600155
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2019-10-29 12:37:17 +0100

    decrement button

--- Week-44/01-tuesday-exercises/demo1/src/App.js
+++ Week-44/01-tuesday-exercises/demo1/src/App.js
@@ -1,4 +1,4 @@
-import React, { usestate } from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 /*
@@ -9,9 +9,20 @@ initially by one, and print the result in the component.
 */
 
 function App() {
+  // State variable count
+  const [count, setCount] = useState(0);
+
   return (
     <div className="App">
-      <p>Place content here</p>
+      <p>The button has been clicked {count} times.</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me to increment by one
+      </button>
+      <br></br>
+      {/*b) Add a new button to the Component which when pressed, should decrement the count value*/}
+      <button onClick={() => setCount(count - 1)}>
+        Click me to decrement by one
+      </button>
     </div>
   );
 }
