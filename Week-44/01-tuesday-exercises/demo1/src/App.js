b9e1708d572aa3d479a4423707112611444318c6
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2019-10-29 13:00:51 +0100

    LocalStorage feature added

--- Week-44/01-tuesday-exercises/demo1/src/App.js
+++ Week-44/01-tuesday-exercises/demo1/src/App.js
@@ -8,19 +8,37 @@ a) It should have a button which when pressed should increment a count value,
 initially by one, and print the result in the component.
 */
 
-function App() {
+/* c) Change the component, so an initial value for count can be passed in via props */
+/* c) Change the component so the value used for increments can be passed in 
+(if you pass in the number five, the count value should increment/decrement with 5 for each click) */
+/*
+d) Add a side effect, using useEffect, that can store the count value in your 
+Browsers Local Storage so that next time the user accesses the page 
+(after having closed the browser) it will render the count value used last time he visited the page
+
+Hint: All modern browsers include a “Local Storage” and “Session Storage” 
+area to store values for the current user or session. Open developer tools and check as sketched here.
+The JavaScript API, used to read/write is extremely simple, you can do like this:
+localStorage.setItem("count", count);
+localStorage.getItem("count") //Returns value as a string
+*/
+function App(props, increment) {
   // State variable count
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(props.initialValue);
+
+  useState(() => {
+    window.localStorage.setItem("count", count);
+  });
 
   return (
     <div className="App">
       <p>The button has been clicked {count} times.</p>
-      <button onClick={() => setCount(count + 1)}>
+      <button onClick={() => setCount(count + increment)}>
         Click me to increment by one
       </button>
       <br></br>
       {/*b) Add a new button to the Component which when pressed, should decrement the count value*/}
-      <button onClick={() => setCount(count - 1)}>
+      <button onClick={() => setCount(count - increment)}>
         Click me to decrement by one
       </button>
     </div>
