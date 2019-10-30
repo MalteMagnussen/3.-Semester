9066b998f7388c242d19c222102c802005585a92
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2019-10-30 16:40:25 +0100

    Delete isn't working yet

--- Week-44/02-wednesday-exercises/LiftingStateUp/src/App.js
+++ Week-44/02-wednesday-exercises/LiftingStateUp/src/App.js
@@ -33,8 +33,40 @@ function App() {
   all state manipulating methods must be placed in App.js, 
   so add these two methods just below the addTodo(..) method:
   */
-  const deleteTodo = index => {};
-  const editTodo = index => {};
+  const deleteTodo = id => {
+    /**
+     * f) Complete the deleteTodo(..) method to remove the selected todo from the list.
+     * Remember it’s not enough to just remove the item from the list.
+     * You must create a clone of the list, remove the todo from this,
+     * and add it to state again, to make React detect the change and then update all dependent Components.
+     * Hint: There are several ways to remove an item from a list.
+     * One of them would be to use the filter method to create a new object including all items from the
+     * original except the one that matches the id for the todo we want to delete
+     */
+    setTodos(
+      todos.filter(todo => {
+        return todo.id !== id;
+      })
+    );
+  };
+  const editTodo = index => {
+    /**
+     * g) Complete the editTodo(id) method, so todo’s can be edited (via the same UI as used for new todo’s)
+     * Hint-1: This requires only two lines of code, since support is already added for edit in the provided code.
+     * All you need to do is, use the provided id to lookup the todo we want to edit,
+     * and then update the nextTodo object in the state object with this todo.
+     * It should then “flow” down into the NewTodo control, and when you press save here,
+     * the save method should detect that this isn’t a new object (id >=0) and handle it accordingly.
+     */
+    const todoEdit = todos.find(element => {
+      return element.id === index;
+    });
+    setNewTodo(todoEdit);
+  };
+  /**
+   * c) Eventually, they will be called from the TodoList presenter-Component,
+   * so pass them down via props together with todos which already are passed down.
+   */
 
   return (
     <div className="container outer">
@@ -44,7 +76,7 @@ function App() {
 
       <div className="row">
         <div className="col-6 allTodos">
-          <TodoList todos={todos} />
+          <TodoList todos={todos} deleteTodo={deleteTodo} editTodo={editTodo} />
         </div>
         <div className="col-5 new-todo">
           <NewTodo addTodo={addTodo} nextTodo={newTodo} />
--- Week-44/02-wednesday-exercises/LiftingStateUp/src/TodoList.js
+++ Week-44/02-wednesday-exercises/LiftingStateUp/src/TodoList.js
@@ -1,13 +1,32 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-const TodoList = ({ todos }) => {
+const TodoList = props => {
+  const { todos, deleteTodo, editTodo } = props;
+  /* 
+  d) In the TodoList component, 
+  change the part that creates each line as sketched below, 
+  to get the links sketched in the initial figure 
+  (feel free to use buttons or whatever you prefer).
+  */
   return (
     <React.Fragment>
       <h2>All Todos</h2>
       <ul>
         {todos.map(todo => (
-          <li key={todo.id}>{todo.todoText}</li>
+          <li key={todo.id}>
+            {todo.todoText}
+            <a
+              href="#/"
+              onClick={(e => e.preventDefault(), deleteTodo(todo.id))}
+            >
+              {" "}
+              (delete{" "}
+            </a>
+            <a href="#/" onClick={() => editTodo(todo.id)}>
+              , edit){" "}
+            </a>
+          </li>
         ))}
       </ul>
     </React.Fragment>
@@ -17,4 +36,4 @@ export default TodoList;
 
 TodoList.propTypes = {
   todos: PropTypes.array
-}
+};
