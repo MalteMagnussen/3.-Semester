a9fe11e4cefd3617987d736ac8b3b8c4b097eb9a
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2019-10-30 17:06:38 +0100

    added initialValue persons

--- Week-44/02-wednesday-exercises/LiftingStateUpPartTwo/src/App.js
+++ Week-44/02-wednesday-exercises/LiftingStateUpPartTwo/src/App.js
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
-
+import uuid from "uuid/v1";
+/**
+ * b) Add the necessary components to render a web-site as sketched in this figure..
+ */
 function App() {
+  const initialValue = [
+    {
+      name: "Malte",
+      id: uuid()
+    },
+    {
+      name: "Asger",
+      id: uuid()
+    },
+    {
+      name: "Camilla",
+      id: uuid()
+    },
+    {
+      name: "Rùni",
+      id: uuid()
+    }
+  ];
+
   return (
     <div className="App">
+      <h1>State Lift Demo</h1>
+      <br />
+      <br />
+      <h2>Total Persons: {persons.length}</h2>
       <p>Place content here</p>
     </div>
   );
