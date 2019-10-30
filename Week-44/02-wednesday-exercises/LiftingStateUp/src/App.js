fc371ae24dbd8c2346dd497ccff434aa9abc2896
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2019-10-30 15:50:19 +0100

    Installed and did a) and b) of Exercise 02 Wednesday

--- Week-44/02-wednesday-exercises/LiftingStateUp/src/App.js
+++ Week-44/02-wednesday-exercises/LiftingStateUp/src/App.js
@@ -1,8 +1,8 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import TodoList from "./TodoList"
-import NewTodo from "./NewTodo"
+import TodoList from "./TodoList";
+import NewTodo from "./NewTodo";
 import uuid from "uuid/v1";
 
 function App() {
@@ -10,26 +10,35 @@ function App() {
     { id: uuid(), todoText: "Wake up" },
     { id: uuid(), todoText: "Make Coffee" },
     { id: uuid(), todoText: "Drink Coffee" }
-  ]
+  ];
   const [todos, setTodos] = useState(initialData);
   const [newTodo, setNewTodo] = useState({ id: "", todoText: "" });
-  console.log(todos)
+  console.log(todos);
 
   const addTodo = todo => {
-    if (todo.id === "") { // id=-1 Indicates a new object
+    if (todo.id === "") {
+      // id=-1 Indicates a new object
       todo.id = uuid();
       todos.push(todo);
-    } else {//if id != "", it's an existing todo. Find it and add changes
+    } else {
+      //if id != "", it's an existing todo. Find it and add changes
       let todoToEdit = todos.find(t => t.id === todo.id);
       todoToEdit.todoText = todo.todoText;
     }
     setTodos([...todos]);
-    setNewTodo({id:"", todoText:""})
+    setNewTodo({ id: "", todoText: "" });
   };
-  
+  /* 
+  b) As outlined in the video given above, 
+  all state manipulating methods must be placed in App.js, 
+  so add these two methods just below the addTodo(..) method:
+  */
+  const deleteTodo = index => {};
+  const editTodo = index => {};
+
   return (
     <div className="container outer">
-      <h2 style={{ textAlign: "center", marginBottom:25 }}>
+      <h2 style={{ textAlign: "center", marginBottom: 25 }}>
         Props and Lifting State Demo
       </h2>
 
@@ -38,10 +47,7 @@ function App() {
           <TodoList todos={todos} />
         </div>
         <div className="col-5 new-todo">
-          <NewTodo
-            addTodo={addTodo}
-            nextTodo={newTodo}            
-          />
+          <NewTodo addTodo={addTodo} nextTodo={newTodo} />
         </div>
       </div>
     </div>
