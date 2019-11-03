061d61d97dfb6bbcfe95542e370f882f356df46d
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2019-11-03 17:05:45 +0100

    Implemented Bootstrap Navbar

--- Week-45/00-monday-exercises/router_demo/src/App.js
+++ Week-45/00-monday-exercises/router_demo/src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import "./App.css";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import Home from "./components/Home";
--- Week-45/00-monday-exercises/router_demo/src/components/Navigation.js
+++ Week-45/00-monday-exercises/router_demo/src/components/Navigation.js
@@ -1,11 +1,19 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { Navbar, Nav } from "react-bootstrap";
 
 const Navigation = () => {
   return (
     <div>
-      <NavLink to="/">Home</NavLink> <NavLink to="/about">About</NavLink>{" "}
-      <NavLink to="/contact">Contact</NavLink>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/contact">Contact</Nav.Link>
+            <Nav.Link href="/about">About</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     </div>
   );
 };
