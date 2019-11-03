b7dc96a30c8202ccd25291df91047f9787fd5a69
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2019-11-03 14:31:31 +0100

    First router working

--- Week-45/00-monday-exercises/router_demo/src/App.js
+++ Week-45/00-monday-exercises/router_demo/src/App.js
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Route } from "react-router-dom";
+
+// Let's create a dummy component
+const NewRoute = () => {
+  return (
+    <div>
+      <p>New Route</p>
+    </div>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Route path="/new" component={NewRoute} />
+    </BrowserRouter>
   );
 }
 
