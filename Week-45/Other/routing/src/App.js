1ad7f1ce66eb6dbf02383437a8ed19b550bbe59a
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2019-11-04 15:57:32 +0100

    Added data.json and initial router

--- Week-45/Other/routing/src/App.js
+++ Week-45/Other/routing/src/App.js
@@ -1,10 +1,5 @@
 import React from "react";
-import {
-  HashRouter as Router,
-  Switch,
-  Route,
-  Link as NavLink
-} from "react-router-dom";
+import { HashRouter as Router, Switch, Route, NavLink } from "react-router-dom";
 
 export default function App() {
   return (
