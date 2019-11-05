675112f0e992825ba4c6b2eba9ab9389af43e0f2
Malte Hviid-Magnussen <maltehviidmagnussen@gmail.com>
2019-11-05 13:42:42 +0100

    Pasted Start Code

--- Week-45/01-tuesday-exercises/react_and_routing/src/App.js
+++ Week-45/01-tuesday-exercises/react_and_routing/src/App.js
@@ -1,9 +1,62 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink
+} from "react-router-dom";
 
 function App() {
-  return <div></div>;
+  return (
+    <Router>
+      <Header />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/products">
+          <Product />
+        </Route>
+        <Route path="/company">
+          <Company />
+        </Route>
+        {/* <Route path="/add-book">
+          <AddBook />
+        </Route> */}
+      </Switch>
+    </Router>
+  );
 }
+
+const Header = () => (
+  <ul className="header">
+    <li>
+      <NavLink exact activeClassName="active" to="/">
+        Home
+      </NavLink>
+    </li>
+    <li>
+      <NavLink activeClassName="active" to="/products">
+        Products
+      </NavLink>
+    </li>
+    <li>
+      <NavLink activeClassName="active" to="/add-book">
+        Add Book
+      </NavLink>
+    </li>
+    <li>
+      <NavLink activeClassName="active" to="/company">
+        Company
+      </NavLink>
+    </li>
+  </ul>
+);
+
+const Home = () => <div>Home</div>;
+const Product = () => <div>Product</div>;
+const Company = () => <div>Company</div>;
 
 export default App;
